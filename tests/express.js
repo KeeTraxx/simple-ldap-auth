1d5a0b53a7725aed459c9bbb47d028190005850e
Khôi Tran <kt@compile.ch>
2014-11-19 18:17:24 +0100

    Switched to much easier usage with express.

--- tests/express.js
+++ tests/express.js
@@ -10,7 +10,7 @@ var session = require('express-session');
 
 var app = express();
 app.use(logger('dev'));
-app.use(session({ secret: 'PEBCAK'}));
+app.use(session({secret: 'PEBCAK'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
@@ -18,23 +18,23 @@ var SimpleLdapAuth = require('../');
 
 app.use('/', SimpleLdapAuth({
     server: {
-        url: 'ldap://ldap.campus.unibe.ch:389',
-        domain: 'CAMPUS',
-        adminDn: 'CN=IWI-inventory,OU=Services,OU=IWI,OU=OU Hosting,DC=campus,DC=unibe,DC=ch',
-        adminPassword: 'bwDG6m7xaeOY6Xw-sLS_',
-        searchBase: 'OU=Employees,OU=PARIS,DC=campus,DC=unibe,DC=ch',
+        url: 'ldap://activedirectory.contoso.com:389',
+        domain: 'CONTOSO',
+        adminDn: 'CN=Administrator,DC=corp,DC=contoso,DC=com',
+        adminPassword: 'somepassword',
+        searchBase: 'OU=Users,DC=corp,DC=contoso,DC=com',
         searchFilter: '(&(objectcategory=person)(objectclass=user)(|(samaccountname={{username}})(mail={{username}})))',
         searchAttributes: ['displayName', 'mail', 'sAMAccountName', 'uid']
     }
 }));
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.send('ok');
 });
 
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
