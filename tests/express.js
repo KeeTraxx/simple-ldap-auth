6aa42a6211a2072c74afb474040c65b0e6441a98
Khôi Tran <kt@compile.ch>
2014-11-19 18:13:16 +0100

    Switched to much easier usage with express.

---
+++ tests/express.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ktran on 19.11.2014.
+ */
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+var app = express();
+app.use(logger('dev'));
+app.use(session({ secret: 'PEBCAK'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+var SimpleLdapAuth = require('../');
+
+app.use('/', SimpleLdapAuth({
+    server: {
+        url: 'ldap://ldap.campus.unibe.ch:389',
+        domain: 'CAMPUS',
+        adminDn: 'CN=IWI-inventory,OU=Services,OU=IWI,OU=OU Hosting,DC=campus,DC=unibe,DC=ch',
+        adminPassword: 'bwDG6m7xaeOY6Xw-sLS_',
+        searchBase: 'OU=Employees,OU=PARIS,DC=campus,DC=unibe,DC=ch',
+        searchFilter: '(&(objectcategory=person)(objectclass=user)(|(samaccountname={{username}})(mail={{username}})))',
+        searchAttributes: ['displayName', 'mail', 'sAMAccountName', 'uid']
+    }
+}));
+
+app.get('/', function(req,res){
+    res.send('ok');
+});
+
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
--- tests/smoke.js
+++ tests/smoke.js
@@ -1,19 +1,36 @@
 /**
  * Created by kt_000 on 03.05.2014.
  */
-var SimpleLdapAuth = require('../');
+var SimpleLdapAuth = require(__dirname + '../lib/SimpleLdapAuth');
 
-new SimpleLdapAuth({},function(err, auth){
-    if ( err ) {
-        console.log(err);
-    } else {
-        auth.authenticate('user', 'password', function(err, res){
-            if ( err ) {
-                console.warn(err.message);
-            } else {
-                console.log(res);
-            }
-        });
-
+var options = {
+    server: {
+        url: 'ldap://ldap.campus.unibe.ch:389',
+        domain: 'CAMPUS',
+        adminDn: 'CN=IWI-inventory,OU=Services,OU=IWI,OU=OU Hosting,DC=campus,DC=unibe,DC=ch',
+        adminPassword: 'bwDG6m7xaeOY6Xw-sLS_',
+        searchBase: 'OU=Employees,OU=PARIS,DC=campus,DC=unibe,DC=ch',
+        searchFilter: '(&(objectcategory=person)(objectclass=user)(|(samaccountname={{username}})(mail={{username}})))',
+        searchAttributes: ['displayName', 'mail', 'sAMAccountName', 'uid']
     }
+};
+
+var auth = new SimpleLdapAuth(options);
+console.log(auth);
+auth.authenticate('ktran', 'SXfvGB2w', function (err, res) {
+    if (err) {
+        console.warn(err.message);
+    } else {
+        console.log(res);
+    }
+
+});
+
+auth.authenticate('kttest', 'SXfvGB2w', function (err, res) {
+    if (err) {
+        console.warn(err.message);
+    } else {
+        console.log(res);
+    }
+
 });
